de25f9d35d432499f1b26abc7d0f168e07281b1d
ngminhloc <ngminhloc312@gmail.com>
2021-10-29 23:33:44 +0700

    update

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,6 +1,63 @@
 #include "SymbolTable.h"
 
+void SymbolTable::insert(string id, string type, bool isStatic) {
+
+}
+
+void SymbolTable::assign(string id, string value) {
+    
+}
+
+void SymbolTable::begin() {
+    
+}
+
+void SymbolTable::end() {
+    
+}
+
+void SymbolTable::lookup(string id) {
+    
+}
+
+void SymbolTable::print() {
+    
+}
+
+void SymbolTable::check(string cmd, string para1, string para2) {
+    
+}
+
 void SymbolTable::run(string filename)
 {
-    cout << "success";
+    ifstream openFile(filename);
+	if (openFile.is_open())
+	{
+		string buffer;
+		while (getline(openFile, buffer)) {
+			string inputString[3] = {"", "", ""};
+			int beginPos = 0, currPos = 0, numOfChar = 0, paramNum = 0, strLen = buffer.length();
+			while (true) {
+				if (currPos >= strLen) {
+					inputString[paramNum++] = buffer.substr(beginPos, numOfChar);
+					break;
+				}
+				if (buffer[currPos] == ' ') {
+					inputString[paramNum++] = buffer.substr(beginPos, numOfChar);
+					beginPos = currPos + 1;
+					numOfChar = 0;
+				}
+				else {
+					numOfChar++;
+				}
+				currPos++;
+			}
+			if (inputString[0] != "")	{
+				check(inputString[0], inputString[1], inputString[2]);
+			}
+		}
+		if (scope > 0)
+			throw UnclosedBlock(scope);
+		openFile.close();
+	}
 }
--- SymbolTable.h
+++ SymbolTable.h
@@ -2,10 +2,25 @@
 #define SYMBOLTABLE_H
 #include "main.h"
 
+class Symbol {
+
+};
+
+class Table {
+    
+};
+
 class SymbolTable
 {
 public:
     SymbolTable() {}
-    void run(string filename);
+    inline void check(string cmd, string param1, string param2);
+	inline void insert(string id, string type, bool isStatic);
+	inline void assign(string id, string value);
+	inline void begin();
+	inline void end();
+	inline void lookup(string id);
+	inline void print();
+	inline void run(string filename);
 };
 #endif
