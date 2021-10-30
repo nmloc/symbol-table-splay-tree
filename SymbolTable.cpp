ac2351312a0571893fd367fe8c59f51364dcf90a
ngminhloc <ngminhloc312@gmail.com>
2021-10-30 16:16:21 +0700

    update

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -1,31 +1,91 @@
 #include "SymbolTable.h"
 
-void SymbolTable::insert(string id, string type, bool isStatic) {
+void SymbolTable::insert(string id, string *paraList, string retType, bool isStatic) {
+	Symbol *node = new Symbol();
+    node->parent = nullptr;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->id = id;
 
+	if (paraList) {
+		for(int i = 0; i<(sizeof(paraList)/sizeof(paraList[0])); i++) {
+            node->paraList[i] = paraList[i];
+        }
+	}
+
+	if (isStatic) node->scopeLevel = 0;
+	else node->scopeLevel = this->currentScope;
+
+    Symbol *y = nullptr;
+    Symbol *x = this->root;
+
+    while (x != nullptr) {
+        y = x;
+        if (node->id.compare(x->id) < 0) {
+            x = x->left;
+        } else {
+            x = x->right;
+        }
+    }
+
+    // y is parent of x
+    node->parent = y;
+    if (y == nullptr) {
+        root = node;
+    } else if (node->id.compare(y->id) < 0) {
+        y->left = node;
+    } else {
+        y->right = node;
+    }
+
+    // splay the node
+    splay(node);
 }
 
 void SymbolTable::assign(string id, string value) {
-    
+    regex rNum("[0-9]+");
+	regex rString("['][ a-zA-Z0-9]*[']");
+	regex rId("[a-z][_a-zA-Z0-9]*");
+	string error = "ASSIGN " + id + " " + value;
 }
 
 void SymbolTable::begin() {
-    
+    this->currentScope++;
 }
 
 void SymbolTable::end() {
-    
+    this->currentScope--;
 }
 
 void SymbolTable::lookup(string id) {
-    
+	Symbol *x = lookupRecursive(this->root, id);
+    if (x) {
+		splay(x);
+	}
+	cout << x->scopeLevel << endl;
 }
 
 void SymbolTable::print() {
-    
+    preOrderTraversal(root);
 }
 
 void SymbolTable::check(string cmd, string para1, string para2) {
-    
+    if (cmd == "INSERT")
+		insert(para1, para2);
+	else if (cmd == "ASSIGN")
+		assign(para1, para2);
+	else if (cmd == "BEGIN")
+		begin();
+	else if (cmd == "END")
+		end();
+	else if (cmd == "LOOKUP")
+		lookup(para1);
+	else if (cmd == "PRINT")
+		print();
+	else {
+		string error = cmd + " " + para1 + " " + para2;
+		throw InvalidInstruction(error);
+	}
 }
 
 void SymbolTable::run(string filename)
@@ -35,7 +95,7 @@ void SymbolTable::run(string filename)
 	{
 		string buffer;
 		while (getline(openFile, buffer)) {
-			string inputString[3] = {"", "", ""};
+			string inputString[4] = {"", "", "", ""};
 			int beginPos = 0, currPos = 0, numOfChar = 0, paramNum = 0, strLen = buffer.length();
 			while (true) {
 				if (currPos >= strLen) {
@@ -56,8 +116,8 @@ void SymbolTable::run(string filename)
 				check(inputString[0], inputString[1], inputString[2]);
 			}
 		}
-		if (scope > 0)
-			throw UnclosedBlock(scope);
+		if (currentScope > 0)
+			throw UnclosedBlock(currentScope);
 		openFile.close();
 	}
 }
--- SymbolTable.h
+++ SymbolTable.h
@@ -3,19 +3,156 @@
 #include "main.h"
 
 class Symbol {
-
-};
-
-class Table {
-    
+public:
+    string id;
+    string paraList[10];
+    string retType;
+    int scopeLevel;
+    Symbol *parent, *left, *right;
 };
 
 class SymbolTable
 {
 public:
+    Symbol *root;
+    int currentScope = 0;
+
+	void leftRotate(Symbol *x) {
+        Symbol *y = x->right;
+		x->right = y->left;
+		if (y->left != nullptr) {
+			y->left->parent = x;
+		}
+		y->parent = x->parent;
+		if (x->parent == nullptr) {
+			this->root = y;
+		} else if (x == x->parent->left) {
+			x->parent->left = y;
+		} else {
+			x->parent->right = y;
+		}
+		y->left = x;
+		x->parent = y;
+	}
+
+	void rightRotate(Symbol *x) {
+		Symbol *y = x->left;
+		x->left = y->right;
+		if (y->right != nullptr) {
+			y->right->parent = x;
+		}
+		y->parent = x->parent;
+		if (x->parent == nullptr) {
+			this->root = y;
+		} else if (x == x->parent->right) {
+			x->parent->right = y;
+		} else {
+			x->parent->left = y;
+		}
+		y->right = x;
+		x->parent = y;
+	}
+    
+	void splay(Symbol *x) {
+		while (x->parent) {
+			if (!x->parent->parent) {
+				if (x == x->parent->left) {
+					// zig rotation
+					rightRotate(x->parent);
+				} else {
+					// zag rotation
+					leftRotate(x->parent);
+				}
+			} else if (x == x->parent->left && x->parent == x->parent->parent->left) {
+				// zig-zig rotation
+				rightRotate(x->parent->parent);
+				rightRotate(x->parent);
+			} else if (x == x->parent->right && x->parent == x->parent->parent->right) {
+				// zag-zag rotation
+				leftRotate(x->parent->parent);
+				leftRotate(x->parent);
+			} else if (x == x->parent->right && x->parent == x->parent->parent->left) {
+				// zig-zag rotation
+				leftRotate(x->parent);
+				rightRotate(x->parent);
+			} else {
+				// zag-zig rotation
+				rightRotate(x->parent);
+				leftRotate(x->parent);
+			}
+		}
+	}
+
+	// joins two trees s and t
+	Symbol *join(Symbol *s, Symbol *t){
+		if (!s) {
+			return t;
+		}
+
+		if (!t) {
+			return s;
+		}
+		Symbol *x = maximum(s);
+		splay(x);
+		x->right = t;
+		t->parent = x;
+		return x;
+	}
+
+	// splits the tree into s and t
+	void split(Symbol *&x, Symbol *&s, Symbol *&t) {
+		splay(x);
+		if (x->right) {
+			t = x->right;
+			t->parent = nullptr;
+		} else {
+			t = nullptr;
+		}
+		s = x;
+		s->right = nullptr;
+		x = nullptr;
+	}
+
+	Symbol* minimum(Symbol *node) {
+		while (node->left != nullptr) {
+			node = node->left;
+		}
+		return node;
+	}
+
+	Symbol* maximum(Symbol *node) {
+		while (node->right != nullptr) {
+			node = node->right;
+		}
+		return node;
+	}
+
+	Symbol* lookupRecursive(Symbol *node, string id) {
+		if (node == nullptr || id.compare(node->id) == 0) {
+			return node;
+		}
+
+		if (id.compare(node->id) < 0) {
+			return lookupRecursive(node->left, id);
+		} 
+		return lookupRecursive(node->right, id);
+	}
+
+	void preOrderTraversal(Symbol *node) {
+		if (node != nullptr) {
+			cout << node->id << endl;
+			preOrderTraversal(node->left);
+			preOrderTraversal(node->right);
+		} 
+	}
+
+	void deleteNode(Symbol *node) {
+		
+	}
+    
     SymbolTable() {}
-    inline void check(string cmd, string param1, string param2);
-	inline void insert(string id, string type, bool isStatic);
+    inline void check(string cmd, string para1, string para2);
+	inline void insert(string id, string *paraList, string retType, bool isStatic);
 	inline void assign(string id, string value);
 	inline void begin();
 	inline void end();
